99c383ae8e5e4b9a0fd2274115838a58ad2e5194
Tarik Desire <tprdesire@gmail.com>
2018-10-03 18:17:42 -0400

    Added things

--- arrayDrillsFunctions.js
+++ arrayDrillsFunctions.js
@@ -8,14 +8,13 @@ function URLify(urlString) {
   const index = urlString.indexOf(' ');
   return urlString.slice(0, index) + '%20' + URLify(urlString.slice(index + 1));
 }
-
 console.log(URLify('tauhida parveen'));
 console.log(URLify('www.thinkful.com /tauh ida parv een'));
 // Runtime: O(n) - Linear
 
 
 function lessThan(arr, num, minIndex = 0) {
-  const sortedArr = arr.sort();
+  const sortedArr = arr.sort((a,b) => a-b);
   if (sortedArr[0] >= num) {
     return arr;
   }
@@ -25,10 +24,59 @@ function lessThan(arr, num, minIndex = 0) {
   }
   return lessThan(arr.slice(minIndex), num);
 }
-
-console.log(lessThan([1, 2, 3, 4, 5, 6, 7, 8, 9], 5));
-
+console.log(lessThan([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
 // Runtime: O(n) - Linear
+
+
+// const maxSumOfArray = array => {
+//   let maxSum = 0;
+
+//   for (let i=0; i<array.length; i++) {
+//     let tempSum = 0;
+//     let endingIndex = i;
+//     for (let j=endingIndex; j < array.length; j++) {
+//       tempSum += array[j];
+//       if (tempSum > maxSum) {
+//         maxSum = tempSum;
+//       }
+//     }
+//   }
+//   return maxSum;
+// };
+
+// console.log(maxSumOfArray([4,6,-3,5,-2,1]));
+//Runtime: o(n^2) - Polynomial
+
+
+function mergeArray(arr1, arr2, arr3 = []) {
+  if (arr1.length === 0 && arr2.length === 0) {
+    return arr3;
+  }
+
+  if (arr1.length === 0 && arr2.length > 0) {
+    return [ ...arr3, ...arr2];
+  }
+
+  if (arr2.length === 0 && arr1.length > 0) {
+    return [...arr3, ...arr1];
+  }
+
+  if (arr1[0] < arr2[0]) {
+    arr3.push(arr1[0]);
+    arr3.push(arr2[0]);
+  }
+
+  if (arr2[0] < arr1[0]) {
+    arr3.push(arr2[0]);
+    arr3.push(arr1[0]);
+  }
+
+  return mergeArray(arr1.slice(0), arr2.slice(0));
+}
+
+console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+
+
 
 
 
